6b4cff6117b753013b1f4f9a86f2e449f1333969
Seoeun Choi <holly101763@gmail.com>
2023-04-28 00:05:21 +0900

    first commit

---
+++ src/Idfinding.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, View, Text, Image, TextInput, TouchableOpacity } from "react-native";
+import React from 'react'
+
+
+const Idfinding = () => {
+  return (
+    <View>
+      <Text>Idfinding</Text>
+    </View>
+  )
+}
+const styles = StyleSheet.create{(
+
+)}
+export default Idfinding
